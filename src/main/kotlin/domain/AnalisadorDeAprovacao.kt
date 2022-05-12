5a8ad79fcbdb150394c792909b6fc89d4c5713e4
Miguel de Mello Carpi <miguelmello@usp.br>
2022-05-12 15:15:24 -0300

    src: main: kotlin: domain: AnalisadorDeAprovacao implementado
    Implementado a classe AnalisadorDeAprovacao com os seguintes métodos:
    - defineCriterio(criterio : CriterioDeAprovacao)
      Nesse método um critério de aprovação é adicionado ao objeto
    
    - fechaBoletim(boletim: Boletim) : BoletimFechado
      Esse método utiliza o critério armazenado no objeto para produzir
      o BoletimFechado que diz se um alune foi aprovado ou não, junto com
      sua média final, média de Eps e média de miniEps.

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,20 @@
 package domain
 
+import domain.criterios.Basico
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
+    var criterio : CriterioDeAprovacao = Basico()
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    fun defineCriterio(criterio : CriterioDeAprovacao) {
+        this.criterio = criterio
+    }
 
+    fun fechaBoletim(boletim : Boletim) : BoletimFechado {
+        return BoletimFechado(
+            boletim.mediaEPs,
+            boletim.mediaMiniEPs,
+            criterio.mediaFinal(boletim),
+            criterio.estaAprovado(boletim))
+    }
 }
